8e870ba8b878294b6a70d4f2fee1bc9c1cc4a3cf
Stefano Pigozzi <ste.pigozzi@gmail.com>
2018-02-10 18:05:29 +0100

    Add timer and canvas

---
+++ Assets/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public double time_left;
+	public bool running;
+	public GameObject display_on;
+
+	void Start () {
+		time_left = 10d;
+	}
+
+	void Update () {
+		if (running) {
+			time_left -= Time.deltaTime;
+			//Stop the timer at 0.000
+			if (time_left <= 0d) {
+				time_left = 0d;
+				running = false;
+			}
+			//Update display before calling the time ran out function
+			if (display_on != null) {
+				display_on.GetComponent<Text> ().text = time_left.ToString("00.000");
+			}
+			//Call the time ran out function
+			if (time_left <= 0d) {
+				TimeRanOut ();
+			}
+		}
+	}
+
+	void TimeRanOut() {
+	}
+}
